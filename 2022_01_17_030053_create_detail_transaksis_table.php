113a4826069585538beeaf61251b586ff940e51b
namiranf23 <88755656+namiranf23@users.noreply.github.com>
2022-01-18 09:20:45 +0700

    Add files via upload

---
+++ 2022_01_17_030053_create_detail_transaksis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_transaksi', 11);
+            $table->foreignId('id_paket', 11);
+            $table->double('qty');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksis');
+    }
+}
